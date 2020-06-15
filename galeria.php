3662e0832cd0383a237349f41e2afd349abc21c5
FILIPE <felipehyuga123@gmail.com>
2020-06-15 18:56:53 -0300

    site

---
+++ galeria.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!--icones-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>clorocine</title>
+</head>
+
+<body>
+    <nav class="nav-extended purple lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">galeria</a></li>
+                <li><a href="cadastrar.php">cadastrar</a></li>
+            </ul>
+
+        </div>
+        <div class="nav-header center">
+            <h1>Clorocine</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active">Todos</a></li>
+                <li class="tab"><a class="active" href="#test2">Assistidos</a></li>
+                <li class="tab "><a class="active">Favoritos</a> </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/original/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
+
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper"><i class="material-icons amber-text">star</i>9,7</p>
+                    <span class="card-title">Vingadores</span>
+                    <p>Após os eventos devastadores de "Vingadores: Guerra Infinita", o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes,
+                     os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.</p>
+
+                </div>
+
+            </div>
+
+        </div>
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/original/bNGW8zYA91VqTZfV3jnKHPEKKvB.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper"><i class="material-icons amber-text">star</i>9,1</p>
+                    <span class="card-title">Parasita</span>
+                    <p>Toda a família de Ki-taek está desempregada, vivendo num porão sujo e apertado. Uma obra do acaso faz com que o filho adolescente da família comece a dar aulas de inglês à garota de uma família rica. Fascinados com a vida luxuosa destas pessoas, pai, mãe, filho e filha bolam um plano para se infiltrarem também na família burguesa, um a um. No entanto,
+                        os segredos e mentiras necessários à ascensão social custarão caro a todos.Bong Joon-ho</p>
+
+
+</body>
+
+</html>
